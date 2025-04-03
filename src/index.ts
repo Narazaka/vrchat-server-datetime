0eccbe03495629820724f1dd1bac71e4b9c3d5fd
Narazaka <info@narazaka.net>
2025-04-03 23:26:19 +0900

    impl

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,10 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const t = `${new Date().getTime()}0000`; // 100 ns unit (C# DateTime.Ticks)
+		if (request.url.endsWith(".json")) {
+			return new Response(`{"t":${t}}`);
+		}
+		return new Response(t);
 	},
 } satisfies ExportedHandler<Env>;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,31 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
+import { SELF } from 'cloudflare:test';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+beforeEach(() => {
+	vi.useFakeTimers();
+})
+
+afterEach(() => {
+	vi.useRealTimers();
+})
+
+describe('worker', () => {
+	it('responds text/plain C# DateTime.Ticks time', async () => {
+		const now = new Date();
+		vi.setSystemTime(now);
+		const response = await SELF.fetch('https://example.com');
+		expect(await response.text()).toEqual(`${now.getTime() * 10000}`);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('responds application/json C# DateTime.Ticks time', async () => {
+		const now = new Date();
+		vi.setSystemTime(now);
+		const response = await SELF.fetch('https://example.com/index.json');
+		expect(await response.json()).toEqual({ t: now.getTime() * 10000 });
 	});
 });
